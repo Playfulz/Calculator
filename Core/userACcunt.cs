64e72dba5ca429f27d5922a780e2f17a8e5f7085
Playful <41659272+Playfulz@users.noreply.github.com>
2020-07-24 16:00:25 -0400

    Update userACcunt.cs

--- Core/userACcunt.cs
+++ Core/userACcunt.cs
@@ -3,23 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using stuf.Commands.Challenges;
 
-namespace stuf.Commands
-{
-    public class UserAccount
-    {
+namespace Calculator.Core {
+    
+    public class UserAccount {
+    
         public ulong ID { get; set; }
-        public uint Cherries { get; set; }
-        public uint XP { get; set; }
-        public uint Lvl
-        {
-            get
-            {
-                return (uint)Math.Sqrt(XP / 5000);
-            }
-        }
-        public uint Baguette { get; set; } 
-        public List<Challenge> ChallengesCompleted { get; set; }
     }
 }
